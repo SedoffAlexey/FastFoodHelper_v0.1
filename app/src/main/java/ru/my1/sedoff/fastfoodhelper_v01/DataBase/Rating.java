ffe4cd7d90298de36664b87e9dc40271536029ac
aleksoider <aleksoider@mail.ru>
2015-02-28 16:14:04 +0300

    Rating CLASS+map sorting by VALUE

--- app/src/main/java/ru/my1/sedoff/fastfoodhelper_v01/DataBase/Rating.java
+++ app/src/main/java/ru/my1/sedoff/fastfoodhelper_v01/DataBase/Rating.java
@@ -3,7 +3,13 @@ package ru.my1.sedoff.fastfoodhelper_v01.DataBase;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -87,6 +93,27 @@ public class Rating {
                     UpdatePref(rating);
             }
     }
+    private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap) {
 
+        // Convert Map to List
+        List<Map.Entry<String, Integer>> list =
+                new LinkedList<>(unsortMap.entrySet());
+
+        // Sort list with comparator, to compare the Map values
+        Collections.sort(list, new Comparator<Map.Entry<String,Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // Convert sorted map back to a Map
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+        for (Iterator<Map.Entry<String, Integer>> it = list.iterator(); it.hasNext();) {
+            Map.Entry<String, Integer> entry = it.next();
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedMap;
+    }
 }
 
